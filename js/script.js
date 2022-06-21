5f8b17c34a955a40bb7ce29aaf86971a72e4fb2c
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-06-21 16:21:48 +0200

    fixed sum 2 decimal places

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const userChosenDistance = parseInt(prompt('quanti km vuoi percorrere?', 256))
-const userAge = parseInt(prompt ('quanti anni hai?',24 ))
+const userAge = parseInt(prompt ('quanti anni hai?', 24 ))
 
 console.log(userChosenDistance)
 console.log(userAge)
@@ -10,15 +10,18 @@ sum = sum.toFixed(2);
 console.log(sum)
 
 if (userAge <= 17){
-    sumOver = Math.floor((sum * 20) / 100);
+    sum = Math.floor(sum * 20 / 100);
+    sum = sum.toFixed(2);
     console.log('hai ricevuto uno sconto del 20%' + '' + sum )
+    
 }else if ( userAge >= 17) {
-    sumAnder = Math.floor((sum * 40) / 100);
+    sum = Math.floor(sum * 40 / 100);
+    sum = sum.toFixed(2);
     console.log('hai ricevuto uno sconto del 40%' + '' + sum)
 }
 
 
 const paragraph = document.getElementById('message')
 
-paragraph .innerText = `il costo del tuo biglietto e di ${sum} `
+paragraph .innerText = `Il costo del tuo biglietto e di: ${sum}  `
 console.log(paragraph)
